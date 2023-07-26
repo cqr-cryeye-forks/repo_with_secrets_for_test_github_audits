131ceee23870063414625dc34e8ebf5d447cee0c
alex-matveenko <matveenko.a.p@gmail.com>
2023-07-26 15:22:49 +0300

    init

---
+++ main.py
@@ -0,0 +1,7 @@
+# API_KEY = "ghp_dKSFnTHg30oFAGkzBIp2z0QxeRqEre3BdRET"
+# API_KEY = "ghp_dsanTHg30oFAGkzBIp2z0QxeRqEre3BdRET"
+# PASSWORD = "p@@@ssword"
+# EMAIL = "test.example@gmail.com"
+git_token = "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"
+if __name__ == "__name__":
+    print("DONE")
